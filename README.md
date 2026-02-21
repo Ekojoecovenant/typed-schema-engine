d621d6a75c96f5690cfe5820e9fa5ef6a2c311e4
Ekojoecovenant <covenantekojoe@gmail.com>
2026-02-21 16:27:07 +0000

    Initial setup: strict TS project + folder structure

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # typed-schema-engine
+
 Brutal mini type-safe query builder / ORM - learning advanced TS generics &amp; inference
---
+++ src/index.ts
@@ -0,0 +1,5 @@
+console.log("Tiny-orm is alive");
+
+type Hello = "Cove is cool";
+const msg: Hello = "Cove is cool";
+console.log(msg);
